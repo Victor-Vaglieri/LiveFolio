b3fd7487557798c373e3c7b7ad03f58d1cb2fd02
Victor-Vaglieri <victor_vglr@hotmail.com>
2026-05-11 12:13:28 -0300

    correção de sincronização com o db

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Iniciar o worker em segundo plano
 php bin/worker.php &
 
-# Definir a porta para o FrankenPHP (O Render passa a porta na variável $PORT)
+php bin/import_github.php &
+
 export FRANKENPHP_HTTP_PORT=${PORT:-8080}
 
-# Iniciar o servidor
 exec frankenphp php-server public/index.php
